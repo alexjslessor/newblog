8c7df35f2df43fab6a1a9196b39d4f8fec48b119
hauwei <alexjslessor>
2019-12-28 00:23:39 -0500

    first comit

---
+++ gatsby-browser.js
@@ -0,0 +1,5 @@
+import "prismjs/themes/prism-tomorrow.css";
+// import "prismjs/themes/prism-okaidia.css";
+// import "prismjs/themes/prism-funky.css";
+// import "prismjs/themes/prism-coy.css";
+// import "prismjs/themes/prism.css";
